9ce085cde0fb9978e13b4daffccd2816bce138be
Tushar Meena <89063728+tmeena202@users.noreply.github.com>
2026-02-15 09:54:46 +0530

    created feed API

--- src/app.js
+++ src/app.js
@@ -9,10 +9,12 @@ app.use(cookieParser());
 const authRouter = require("./routers/auth");
 const profileRouter = require("./routers/profile");
 const requestRouter = require("./routers/request");
+const userRouter = require("./routers/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
+app.use("/", userRouter);
 
 connectDB()
   .then(() => {
